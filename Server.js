ea802e6b0d2e776f022439695fae2e03a332a907
Jullia <jullia.azevedo123@gmai.com>
2024-05-25 11:26:40 -0300

    Adiciona modelo de dados e controlador

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const routes = require("./routes/ToDoRoute");
 
@@ -7,6 +8,10 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
 
 mongoose
   .connect(process.env.MONGODB_URL)
